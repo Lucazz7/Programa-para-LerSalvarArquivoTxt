7d559a0267bd4922eca6e76e39ab9ea4b770189c
Lucazz7 <lucascarls002@gmail.com>
2021-04-22 11:05:14 -0300

    Adicionar arquivos de projeto.

---
+++ LerSalvarArquivoTxt/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace LerSalvarArquivoTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_salvar = new System.Windows.Forms.Button();
+            this.bt_ler = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxSalvar = new System.Windows.Forms.RichTextBox();
+            this.textBoxEndereco = new System.Windows.Forms.TextBox();
+            this.richTextBoxLer = new System.Windows.Forms.RichTextBox();
+            this.textBoxEnderecoler = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_salvar
+            // 
+            this.bt_salvar.Location = new System.Drawing.Point(52, 292);
+            this.bt_salvar.Name = "bt_salvar";
+            this.bt_salvar.Size = new System.Drawing.Size(152, 36);
+            this.bt_salvar.TabIndex = 0;
+            this.bt_salvar.Text = "Salvar arquivo";
+            this.bt_salvar.UseVisualStyleBackColor = true;
+            this.bt_salvar.Click += new System.EventHandler(this.bt_salvar_Click);
+            // 
+            // bt_ler
+            // 
+            this.bt_ler.Location = new System.Drawing.Point(298, 334);
+            this.bt_ler.Name = "bt_ler";
+            this.bt_ler.Size = new System.Drawing.Size(141, 29);
+            this.bt_ler.TabIndex = 1;
+            this.bt_ler.Text = "Ler Arquivo";
+            this.bt_ler.UseVisualStyleBackColor = true;
+            this.bt_ler.Click += new System.EventHandler(this.bt_ler_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite texto na area abaixo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ler e mostrar arquivo";
+            // 
+            // richTextBoxSalvar
+            // 
+            this.richTextBoxSalvar.Location = new System.Drawing.Point(41, 44);
+            this.richTextBoxSalvar.Name = "richTextBoxSalvar";
+            this.richTextBoxSalvar.Size = new System.Drawing.Size(179, 242);
+            this.richTextBoxSalvar.TabIndex = 4;
+            this.richTextBoxSalvar.Text = "";
+            // 
+            // textBoxEndereco
+            // 
+            this.textBoxEndereco.Location = new System.Drawing.Point(12, 343);
+            this.textBoxEndereco.Name = "textBoxEndereco";
+            this.textBoxEndereco.ReadOnly = true;
+            this.textBoxEndereco.Size = new System.Drawing.Size(219, 20);
+            this.textBoxEndereco.TabIndex = 5;
+            // 
+            // richTextBoxLer
+            // 
+            this.richTextBoxLer.Location = new System.Drawing.Point(281, 70);
+            this.richTextBoxLer.Name = "richTextBoxLer";
+            this.richTextBoxLer.ReadOnly = true;
+            this.richTextBoxLer.Size = new System.Drawing.Size(183, 258);
+            this.richTextBoxLer.TabIndex = 6;
+            this.richTextBoxLer.Text = "";
+            // 
+            // textBoxEnderecoler
+            // 
+            this.textBoxEnderecoler.Location = new System.Drawing.Point(257, 44);
+            this.textBoxEnderecoler.Name = "textBoxEnderecoler";
+            this.textBoxEnderecoler.ReadOnly = true;
+            this.textBoxEnderecoler.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxEnderecoler.Size = new System.Drawing.Size(223, 20);
+            this.textBoxEnderecoler.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 385);
+            this.Controls.Add(this.textBoxEnderecoler);
+            this.Controls.Add(this.richTextBoxLer);
+            this.Controls.Add(this.textBoxEndereco);
+            this.Controls.Add(this.richTextBoxSalvar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_ler);
+            this.Controls.Add(this.bt_salvar);
+            this.Name = "Form1";
+            this.Text = "Ler e Salvar Arquivo Texto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_salvar;
+        private System.Windows.Forms.Button bt_ler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBoxSalvar;
+        private System.Windows.Forms.TextBox textBoxEndereco;
+        private System.Windows.Forms.RichTextBox richTextBoxLer;
+        private System.Windows.Forms.TextBox textBoxEnderecoler;
+    }
+}
+
---
+++ LerSalvarArquivoTxt/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LerSalvarArquivoTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_salvar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Arquivo de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
+            saveFileDialog1.Title = "Salvar um Arquivo Texto";
+            saveFileDialog1.ShowDialog();
+
+            if(saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                byte[] bytes = Encoding.ASCII.GetBytes(richTextBoxSalvar.Text);
+                int Nbytes = Encoding.ASCII.GetCharCount(bytes);
+                fs.Write(bytes, 0, Nbytes);
+                //Escrever o endereco do arquivo
+                textBoxEndereco.Text = saveFileDialog1.FileName;
+                fs.Close();
+            }
+        }
+
+        private void bt_ler_Click(object sender, EventArgs e)
+        {
+            byte[] arqbin = new byte[1024];
+            string arqtxt;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+           
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var sr = new StreamReader(openFileDialog1.FileName);
+                openFileDialog1.Filter = "Arquivo de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
+                openFileDialog1.Title = "Abrir arquivo";
+                arqtxt = sr.ReadToEnd();
+                sr.Close();
+
+                // Ler arquivos no text box
+                richTextBoxLer.Text = arqtxt;
+                // Ler o endereço do arquvio
+                textBoxEnderecoler.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
